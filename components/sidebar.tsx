a81720c292b93a70fef4fb1a61584c31537e9875
Ben D <bendickinson2002@gmail.com>
2024-06-27 13:17:38 +1000

    fixed package discrepancy

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,12 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import {
-    ClerkLoading,
-    ClerkLoaded,
-    UserButton,
-} from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
-import { Loader } from "lucide-react";
 import { SidebarItem } from "./sidebar-item";
 
 type Props = {
@@ -18,7 +12,7 @@ export const Sidebar = ({ className }: Props) => {
         <div className={cn(
             "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
             className,
-            )}>
+        )}>
             <Link href="/learn">
                 <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
                     <Image src="/mascot.svg" height={40} width={40} alt="Mascot"/>
@@ -32,14 +26,6 @@ export const Sidebar = ({ className }: Props) => {
                 <SidebarItem label="Leaderboard" iconSrc="/leaderboard.svg" href="/leaderboard" />
                 <SidebarItem label="Quests" iconSrc="/quests.svg" href="/quests" />
                 <SidebarItem label="Shop" iconSrc="/shop.svg" href="/shop" />
-            </div>
-            <div className="p-4">
-                <ClerkLoading>
-                    <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
-                </ClerkLoading>
-                <ClerkLoaded>
-                    <UserButton afterSignOutUrl="/" />
-                </ClerkLoaded>
             </div>
         </div>
     );
